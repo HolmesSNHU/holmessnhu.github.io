f9a22a3bf776f3051b8453488324225d3ff444a4
Jason Holmes <jason.holmes3@snhu.edu>
2023-12-09 14:17:28 +0900

    * Updated README with self-assessment and code review. * Moved unit testing from Security to the SecurityTestDriver * Renamed CS499_Security to be in line with the rest of the project's naming conventions: ClientDataSecurity * Added info headers to the data generation scripts.

--- SecurityTestDriver.py
+++ SecurityTestDriver.py
@@ -12,11 +12,74 @@
 # 
 # **************************************************
 
-from CS499_Security import SecurityLayer
+from ClientDataSecurity import SecurityLayer
 
 def main():
+    # Initialize the security layer.
     securityLayer = SecurityLayer()
-    securityLayer.RunTests()
+    
+    # Run a series of simple unit tests for functionality testing.
+        
+    print("Running tests.")
+    print("Test 0: Adding test user...")
+    # Temporary testing variables.
+    self.tempUsername = "admin"
+    self.tempPassword = "root"
+    securityLayer.AddTestUser(self.tempUsername, self.tempPassword)
+    
+    print("Test 1: Testing user verification...")
+    verifiedUser = securityLayer.VerifyUser(self.tempUsername)
+    if verifiedUser:
+        print(f"Verification passed for {self.tempUsername}")
+    else:
+        print(f"Verification failed for {self.tempUsername}")
+    
+    print("Test 2: Testing password hashing...")
+    hashedPassword = securityLayer.HashPassword(self.tempPassword)
+    if securityLayer.VerifyPassword(hashedPassword, verifiedUser.get("hashed_password")):
+        print(f"Hashed password verification passed for {self.tempUsername}")
+    else:
+        print(f"Hashed password verification failed for {self.tempUsername}")
+        
+    print("Test 3: Testing authentication...")
+    authenticated = securityLayer.AuthenticateUser(self.tempUsername, self.tempPassword)
+    if authenticated:
+        print(f"Authentication passed for {self.tempUsername}")
+    else:
+        print(f"Authentication failed for {self.tempUsername}")
+        
+    print("Test 4: Testing session generation...")
+    session = securityLayer.LoginSuccess(self.tempUsername)
+    if session:
+        print(f"Session generation passed for {self.tempUsername}")
+        print(f"Session details: UUID: '{session['UUID']}', token: '{session['token']}'")
+    else:
+        print(f"Session generation failed for {self.tempUsername}")
+        
+    print("Test 5: Testing session validation...")
+    sessionValidated = securityLayer.ValidateSession(session["UUID"], session["token"])
+    if sessionValidated:
+        print(f"Session Authentication passed for {self.tempUsername}")
+    else:
+        print(f"Session Authentication failed for {self.tempUsername}")
+        
+    print("Test 6: Testing account lock...")
+    securityLayer.AccountLock(self.tempUsername, True)
+    verifiedUser = securityLayer.VerifyUser(self.tempUsername)
+    locked = securityLayer.GetAccountLocked(verifiedUser)
+    if locked:
+        print(f"Account locking passed for {self.tempUsername}")
+    else:
+        print(f"Account locking failed for {self.tempUsername}")
+        
+    print("Test 7: Testing account unlock...")
+    securityLayer.AccountLock(self.tempUsername, False)
+    verifiedUser = securityLayer.VerifyUser(self.tempUsername)
+    locked = securityLayer.GetAccountLocked(verifiedUser)
+    if not locked:
+        print(f"Account unlocking passed for {self.tempUsername}")
+    else:
+        print(f"Account unlocking failed for {self.tempUsername}")
 
 if __name__ == "__main__":
     main()
